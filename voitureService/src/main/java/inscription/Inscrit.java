f8a1c6249d86b989fb3ce3d6cd077a2f947021ce
Jeremy-BOURDIOL <jerem.dante@gmail.com>
2013-03-28 22:49:54 +0100

    Ajout de la communication avec couchdb pour stocker les utilisateurs en base

--- voitureService/src/main/java/inscription/Inscrit.java
+++ voitureService/src/main/java/inscription/Inscrit.java
@@ -3,10 +3,15 @@ package inscription;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 import java.util.regex.Pattern;
 
 import openstreetmap.Coordonnee;
 import openstreetmap.OpenStreetMap;
+
+import org.jcouchdb.db.*;
+import org.jcouchdb.document.BaseDocument;
+import org.jcouchdb.exception.NotFoundException;
 
 public class Inscrit {
     private String nom;
@@ -15,7 +20,7 @@ public class Inscrit {
     private String adresse;
     private Coordonnee coordonnee;
     private static Map<String, Inscrit> inscrits = new HashMap<String, Inscrit>();
-    private Inscrit(String nom, String prenom, String mail, String adresse, Coordonnee coordonnee) {
+    private Inscrit(String prenom, String nom, String mail, String adresse, Coordonnee coordonnee) {
         super();
         this.nom = nom;
         this.prenom = prenom;
@@ -71,11 +76,15 @@ public class Inscrit {
              + "   }"
              + " }";
     }
-    public static String nouvelInscrit(String nom, String prenom, String mail, String adresse) {
-        if (!isEmailUps(mail)) {
+    public static String nouvelInscrit(String prenom, String nom, String mail, String adresse) {
+    	// create a database object pointing to the database "mycouchdb" on the local host    
+        //Database db = new Database("localhost", "covoiturage");
+        
+    	if (!isEmailUps(mail)) {
             //code erreur 110 : Adresse email invalide ;
             return message("KO", 110, "Adresse email invalide.");
         }
+        
         if (inscrits.containsKey(mail)) {
             //code erreur 100 : Adresse email déjà utilisée ;
             return message("KO", 100, "Adresse email déjà utilisée.");
@@ -85,8 +94,36 @@ public class Inscrit {
             //code erreur 200 : Adresse postale non connue de Open Street Map.
             return message("KO", 200, "Adresse postale non connue dans Open Street Map.");
         }
-        inscrits.put(mail, new Inscrit(nom, prenom, mail, adresse, coordonnee));
+        inscrits.put(mail, new Inscrit(prenom, nom, mail, adresse, coordonnee));
         System.out.println("Inscription " + mail + " Réussie!");
+
+        /*-----------------------------------------------------------------------
+    	boolean exist = true;
+        try {
+        	db.getDocument(BaseDocument.class, nom+"."+prenom).getId();
+        }
+        catch(NotFoundException e) {
+        	exist = false;
+        }
+        if (exist) {
+            //code erreur 666 : existe en base
+            return message("KO", 666, "On vous connait !");
+        }
+
+        // document for couchdb
+        BaseDocument doc = new BaseDocument();
+        doc.setProperty("_id", nom+"."+prenom);
+        doc.setProperty("nom", nom);
+        doc.setProperty("prenom", prenom);
+        doc.setProperty("email", mail);
+        doc.setProperty("adresse", adresse);
+        Vector<Double> coord = new Vector<Double>(2);
+        coord.add(coordonnee.getLat());
+        coord.add(coordonnee.getLng());
+        doc.setProperty("coordonne",  coord);
+        // create the document in couchdb
+        db.createDocument(doc);
+        -----------------------------------------------------------------------*/
         return message("OK", 0, "Inscription réussie.");
     }
     private static boolean isEmailUps(String email){
--- voitureService/src/main/java/localisation/Localisation.java
+++ voitureService/src/main/java/localisation/Localisation.java
@@ -1,6 +1,5 @@
 package localisation;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 import inscription.Inscrit;
